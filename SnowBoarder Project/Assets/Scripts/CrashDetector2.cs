de4704c5ccf104ff2784b5285c5a896586ffc428
DustFinger2110 <furkansafaaltunyuva@gmail.com>
2022-05-19 22:06:00 +0300

    Added the project SnowBoarder

---
+++ SnowBoarder Project/Assets/Scripts/CrashDetector2.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CrashDetector2 : MonoBehaviour
+{
+    [SerializeField] float delay = 0.5f;
+    [SerializeField] ParticleSystem crashEffect;
+    [SerializeField] AudioClip crashSFX;
+    bool hasCrashed = false;
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+     if (other.tag == "Ground" && !hasCrashed) 
+     {
+         hasCrashed = true;
+         FindObjectOfType<PlayerController>().DisableControls();
+         crashEffect.Play();
+         GetComponent<AudioSource>().PlayOneShot(crashSFX);
+         Invoke("ReloadScene", delay);
+               
+     }  
+
+    }
+       void ReloadScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
